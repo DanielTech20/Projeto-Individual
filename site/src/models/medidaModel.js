6b667bce8d5dbee030cc7b7863a57b5753376d13
DanielTech20 <daniel.teixeira@sptech.school>
2022-11-30 02:26:33 -0300

    Plotando resultado no gráfico

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -1,27 +1,23 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas() {
 
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top ${limite_linhas}
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        momento,
-                        FORMAT(momento, 'HH:mm:ss') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc`;
+        instrucaoSql = `SELECT count(preferido) humano, (SELECT count(preferido) corredor FROM inimigo WHERE preferido = 'corredor') corredor,
+        (SELECT count(preferido) perseguidor FROM inimigo WHERE preferido = 'perseguidor') perseguidor,
+        (SELECT count(preferido) estalador FROM inimigo WHERE preferido = 'estalador') estalador,
+        (SELECT count(preferido) verme FROM inimigo WHERE preferido = 'verme') verme
+        FROM inimigo WHERE preferido = 'humano';
+    `;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+        instrucaoSql = `SELECT count(preferido) humano, (SELECT count(preferido) corredor FROM inimigo WHERE preferido = 'corredor') corredor,
+        (SELECT count(preferido) perseguidor FROM inimigo WHERE preferido = 'perseguidor') perseguidor,
+        (SELECT count(preferido) estalador FROM inimigo WHERE preferido = 'estalador') estalador,
+        (SELECT count(preferido) verme FROM inimigo WHERE preferido = 'verme') verme
+        FROM inimigo WHERE preferido = 'humano';
+    `;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -31,38 +27,8 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
-
-    instrucaoSql = ''
-
-    if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc`;
-
-    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
-    } else {
-        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-        return
-    }
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
 
 
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
 }
